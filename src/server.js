83088807533b26a772e608b421c8c8163e6519ec
trigonal-bacon <121906533+trigonal-bacon@users.noreply.github.com>
2023-01-04 02:07:58 +0000

    > Tidied up server code > Starting on clientside

--- src/server.js
+++ src/server.js
@@ -1,36 +1,39 @@
 import { requestDate } from "./scraper.js";
 import express from "express";
 import { createServer } from "http";
-import cors from "cors";
 import bodyParser from "body-parser";
 import { existsSync, readFileSync } from "fs";
-const app = express();
-app.use(bodyParser.text());
-app.post("/", (req, res) => {
+class Server {
+  constructor() {
+    this.app = express();
+    this.app.use(bodyParser.text());
+  }
+  addGetReq(path, file, type) {
+    this.app.get(path, (_, res) => {
+      res.setHeader("Content-Type", `${type}; charset=utf-8`);
+      if (file && existsSync(file)) {
+        res.writeHead(200);
+        return res.end(readFileSync(file));
+      }
+      res.writeHead(404);
+    });
+    return this;
+  }
+  addPostReq(path, cb) {
+    this.app.post(path, cb);
+    return this;
+  }
+}
+const CINDIServer = new Server()
+.addGetReq("/","client/CINDI.html","text/html")
+.addGetReq("/base.js","client/base.js","application/javascript")
+.addGetReq("/style.css","client/style.css","text/css")
+.addPostReq("/", (req, res) => {
   const args = req.body.split(" ");
   const m = args[0] | 0,
     d = args[1] | 0,
     y = args[2] | 0;
   res.send(requestDate(m, d, y));
 });
-app.get("/", (_, res) => {
-  const file = "client/CINDI.html";
-  res.setHeader("Content-Type", `text/html; charset=utf-8`);
-  if (file && existsSync(file)) {
-    res.writeHead(200);
-    return res.end(readFileSync(file));
-  }
-  res.writeHead(404);
-})
-app.get("/index.js", (_, res) => {
-  const file = "client/base.js";
-  res.setHeader("Content-Type", `application/javascript; charset=utf-8`);
-  if (file && existsSync(file)) {
-    res.writeHead(200);
-    return res.end(readFileSync(file));
-  }
-  res.writeHead(404);
-})
-const server = createServer(app);
+const server = createServer(CINDIServer.app);
 server.listen(8080);
-console.log(app);
